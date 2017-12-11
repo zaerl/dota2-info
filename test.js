53d8f7d4eb6ec9d0a064db5de7122cefe960bee5
Francesco Bigiarini <francesco.bigiarini@gmail.com>
2017-12-11 17:39:05 +0100

    Update tests

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ import m from './lib';
 
 test('get heroes', t => {
   t.true(Array.isArray(m.getHeroes()));
-  t.is(m.getHeroes().length, 113);
+  t.is(m.getHeroes().length, 115);
 });
 
 test('get hero by ID', t => {
@@ -26,8 +26,8 @@ test('get heroes by name', t => {
 });
 
 test('get heroes by role', t => {
-  t.is(m.getHeroes({ roles: { carry: 1 } }).length, 62);
-  t.is(m.getHeroes({ roles: { carry: 2 } }).length, 41);
+  t.is(m.getHeroes({ roles: { carry: 1 } }).length, 63);
+  t.is(m.getHeroes({ roles: { carry: 2 } }).length, 42);
   t.is(m.getHeroes({ roles: { carry: 3 } }).length, 13);
   t.is(m.getHeroes({ roles: { jungler: 1 } }).length, 15);
   t.is(m.getHeroes({ roles: { jungler: 3 } }).length, 4);
@@ -35,8 +35,8 @@ test('get heroes by role', t => {
 });
 
 test('get heroes by attack type', t => {
-  t.is(m.getHeroes({ attack: 'melee' }).length, 54);
-  t.is(m.getHeroes({ attack: 'ranged' }).length, 59);
+  t.is(m.getHeroes({ attack: 'melee' }).length, 55);
+  t.is(m.getHeroes({ attack: 'ranged' }).length, 60);
 });
 
 test('get heroes by code', t => {
@@ -45,8 +45,8 @@ test('get heroes by code', t => {
 
 test('get heroes by primary attribute', t => {
   t.is(m.getHeroes({ primaryAttribute: 'strength' }).length, 37);
-  t.is(m.getHeroes({ primaryAttribute: 'agility' }).length, 36);
-  t.is(m.getHeroes({ primaryAttribute: 'intellect' }).length, 40);
+  t.is(m.getHeroes({ primaryAttribute: 'agility' }).length, 37);
+  t.is(m.getHeroes({ primaryAttribute: 'intellect' }).length, 41);
 });
 
 test('get heroes mixed', t => {
